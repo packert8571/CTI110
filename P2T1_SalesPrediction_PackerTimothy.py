de6dd49908c8740eed6d7172645819ae93c8d7b4
packert8571 <67319781+packert8571@users.noreply.github.com>
2020-06-23 07:05:05 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_PackerTimothy.py
@@ -0,0 +1,15 @@
+print('Get the total projected sales.')
+print('06/21/2020')
+print('CTI-110 P2T1 - Sales Prediction')
+print('Timothy Packer')
+
+
+
+# Get the projected total sales.
+total_sales = float(input('enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
